f9d1f7a18a123215b4b01a39820542e86d13b7c9
Frank <79919504+frank-werner@users.noreply.github.com>
2025-03-05 15:23:51 +0100

    Update main.py
    fixing wrong comments for copyright note

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-/#
+#
 # SPDX-FileName: Material-Prediction-API-Service-v1.0
 # SPDX-FileCopyrightText: Copyright 2010 - 2025 Software GmbH, Darmstadt, Germany and/or its subsidiaries and/or its affiliates
 # SPDX-License-Identifier: Apache-2.0
 #
-#/
+#
 
 #from typing import Union
 
